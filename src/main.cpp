58e67aa24dd0b0bab66ffbdceb8728e90c1b49b6
Victoria Faiola <victoriafaiola2000@gmail.com>
2024-12-18 16:40:38 -0300

    quinto commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,46 @@
-//example
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+#include <ArduinoJson.h>
+#include "app.h"
+#include "cert.h"
+#include "commands.h"
+#include "config.h"
+#include "functions.h"
+#include "internet.h"
+#include "measures.h"
+#include "mqtt.h"
+#include "oled.h"
+#include "persist.h"
+#include "publish.h"
+#include "relay.h"
+#include "setup.h"
+
+
+// Variables
+int maxTemp = 25;       // Temperature limit default
+int maxHumidity = 65;   // Humidity limit default
+bool fanState = false;  // Fan state default (off)
+
+// Principal function
+void control_fan(int TEMPERATURE, int HUMIDITY) {
+    if (TEMPERATURE > maxTemp || HUMIDITY > maxHumidity) {
+        relay_on(); 
+        Serial.println("Fan on"); 
+    }
+    else {
+        relay_off(); 
+        Serial.println("Fan off"); 
+    }
+}
+
+void setup() {
+    measures_init;
+    relay_init;
+    oled88_init;
+    wifi_init;
+    publish_measures;
+    publish_relay;
+}
